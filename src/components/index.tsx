06fd7da9c68f44d990eef089b88949a8549fbf22
HIMDanny <danilbliz0201@gmail.com>
2023-06-12 18:52:36 +0300

    feat: export components from folder

---
+++ src/components/index.tsx
@@ -0,0 +1,17 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import RecipesList from './Recipes/RecipesList';
+import RecipeCard from './Recipes/RecipeCard';
+import Button from './Button';
+import IngredientItem from './Ingredients/IngredientItem';
+import IngredientsList from './Ingredients/IngredientsList';
+
+export {
+  Navbar,
+  Footer,
+  RecipesList,
+  RecipeCard,
+  Button,
+  IngredientItem,
+  IngredientsList,
+};
