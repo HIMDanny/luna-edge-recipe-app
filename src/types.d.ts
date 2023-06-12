24b16c5e60d854894c5f72528c64461df551ff97
HIMDanny <danilbliz0201@gmail.com>
2023-06-12 15:50:14 +0300

    feat: add global types

---
+++ src/types.d.ts
@@ -0,0 +1,31 @@
+type Ingredient = {
+  text: string;
+  quantity: number;
+  foodId: string;
+  image: string;
+  foodCategory: string;
+  food: string;
+  measure: string | null;
+};
+
+type RecipeImagePoperties = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+type Recipe = {
+  uri: string;
+  label: string;
+  images: {
+    THUMBNAIL: RecipeImagePoperties;
+    SMALL: RecipeImagePoperties;
+    REGULAR: RecipeImagePoperties;
+    LARGE: RecipeImagePoperties;
+  };
+  ingredients: Ingredient[];
+  totalTime: number;
+  cuisineType: string[];
+  mealType: string[];
+  dishType: string[];
+};
