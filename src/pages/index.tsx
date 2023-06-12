2b4a4d21e459f55ebdac92f41be0d73d1c25439a
HIMDanny <danilbliz0201@gmail.com>
2023-06-12 18:48:32 +0300

    feat: export pages from folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import RootLayout from './root-layout';
+import Home from './home';
+import Recipe from './recipe';
+import Login from './login';
+import Favourites from './favourites';
+
+export { RootLayout, Home, Recipe, Login, Favourites };
