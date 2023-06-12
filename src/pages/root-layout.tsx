2cb03bbfddfc2c6a00b8f0bf9a26e08aec759ef3
HIMDanny <danilbliz0201@gmail.com>
2023-06-12 15:51:53 +0300

    feat: create root layout

---
+++ src/pages/root-layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import { Footer, Navbar } from '../components';
+
+const RootLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+export default RootLayout;
