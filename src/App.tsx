a1d9eea8a62f8937d1840cef8249efb76e823c1b
HIMDanny <danilbliz0201@gmail.com>
2023-06-12 19:35:53 +0300

    feat: add loaders to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Favourites, Home, Login, Recipe, RootLayout } from './pages';
+import { loader as recipeLoader } from './pages/recipe';
+import { loader as homeLoader } from './pages/home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { index: true, element: <Home /> },
-      { path: ':recipeId', element: <Recipe /> },
+      { index: true, loader: homeLoader, element: <Home /> },
+      { path: ':recipeId', loader: recipeLoader, element: <Recipe /> },
       { path: 'login', element: <Login /> },
       { path: 'favourites', element: <Favourites /> },
       { path: 'sign-up', element: <div>Sign Up</div> },
